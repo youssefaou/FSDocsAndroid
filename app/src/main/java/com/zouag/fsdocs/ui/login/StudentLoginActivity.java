07142c63387737858558771a473a1add11fb425d
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-02-29 20:03:29 +0000

    Implemented a ViewSwitcher to navigate between the login & signup interfaces

--- app/src/main/java/com/zouag/fsdocs/ui/login/StudentLoginActivity.java
+++ app/src/main/java/com/zouag/fsdocs/ui/login/StudentLoginActivity.java
@@ -1,31 +1,30 @@
 package com.zouag.fsdocs.ui.login;
 
-import android.content.Intent;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.zouag.fsdocs.R;
-import com.zouag.fsdocs.ui.signup.StudentSignupActivity;
+import com.zouag.fsdocs.ui.signup.fragments.StudentFrag1;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class StudentLoginActivity extends AppCompatActivity {
 
-    @Bind(R.id.usernameText)
-    EditText usernameText;
-    @Bind(R.id.passwordText)
-    EditText passwordText;
-    @Bind(R.id.signinButtonE)
-    Button signinButton;
-    @Bind(R.id.switchToTeacherBtn)
-    Button switchToTeacherBtn;
-    @Bind(R.id.signupButtonE)
-    Button signupButton;
+    private boolean isSignin = true;
+
+    @Bind(R.id.viewSwitcher)
+    ViewSwitcher mViewSwitcher;
+    @Bind(R.id.signinLabel)
+    TextView mSigninTextview;
+    @Bind(R.id.signupLabel)
+    TextView mSignupTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +32,31 @@ public class StudentLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student_login);
         ButterKnife.bind(this); // Butterknife's setup
 
-        switchToTeacherBtn.setOnClickListener(v -> {
-            Intent mIntent = new Intent(StudentLoginActivity.this, TeacherLoginActivity.class);
-            startActivity(mIntent);
+        mSigninTextview.setOnClickListener(v -> {
+            if (!isSignin) {
+                mViewSwitcher.showPrevious();
+                isSignin = true;
+                mSignupTextview.setBackgroundColor(getResources().getColor(R.color.inactiveTab));
+                mSigninTextview.setBackgroundColor(getResources().getColor(R.color.activeTab));
+            }
+        });
+        mSignupTextview.setOnClickListener(v -> {
+            if (isSignin) {
+                mViewSwitcher.showNext();
+                isSignin = false;
+                mSignupTextview.setBackgroundColor(getResources().getColor(R.color.activeTab));
+                mSigninTextview.setBackgroundColor(getResources().getColor(R.color.inactiveTab));
+            }
         });
 
-        usernameText.setOnEditorActionListener((v, actionId, event) -> {
+        displayFragment(0);
+
+        /*switchToTeacherBtn.setOnClickListener(v -> {
+            Intent mIntent = new Intent(StudentLoginActivity.this, TeacherLoginActivity.class);
+            startActivity(mIntent);
+        });*/
+
+        /*usernameText.setOnEditorActionListener((v, actionId, event) -> {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 testvalide();
                 return true;
@@ -52,18 +70,18 @@ public class StudentLoginActivity extends AppCompatActivity {
                 return true;
             }
             return false;
-        });
+        });*/
 
-        signinButton.setOnClickListener(v -> testvalide());
+        /*signinButton.setOnClickListener(v -> testvalide());
 
         signupButton.setOnClickListener(v -> {
             Intent mIntent = new Intent(StudentLoginActivity.this, StudentSignupActivity.class);
             startActivity(mIntent);
-        });
+        });*/
     }
 
     private void testvalide() {
-        String Usernam = usernameText.getText().toString();
+        /*String Usernam = usernameText.getText().toString();
         String Password = passwordText.getText().toString();
 
         if (TextUtils.isEmpty(Password)) {
@@ -72,7 +90,25 @@ public class StudentLoginActivity extends AppCompatActivity {
         }
         if (TextUtils.isEmpty(Usernam)) {
             usernameText.setError(getString(R.string.erreur));
-        }
+        }*/
     }
 
+    private void displayFragment(int position) {
+        Fragment fragment = null;
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+
+        switch (position) {
+            case 0:
+                fragment = new StudentFrag1();
+                break;
+        }
+
+        ft.replace(R.id.fragment_container, fragment, "fragment" + position);
+        ft.commit();
+    }
+
+    public void processHover() {
+
+    }
 }
